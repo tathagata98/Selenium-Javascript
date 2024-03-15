127598d94e0d5f4e51e300675beab7a9c6bea596
Tathagata Pal <tathagatapal12@gmail.com>
2024-03-15 22:54:53 +0530

    push

--- seleniumfirstrun.js
+++ seleniumfirstrun.js
@@ -5,9 +5,9 @@ async function loginTest() {
   let driver = await new Builder().forBrowser("chrome").build();
   try {
     //navigate to facebook login page
-    await driver.get("");
+    await driver.get("https://www.javatpoint.com/");
     // Select input elements and fill them out
-    await driver.findElement(By.id("email")).sendKeys("test3@gmail.com");
+    /*await driver.findElement(By.id("email")).sendKeys("test3@gmail.com");
     await driver.findElement(By.id("password")).sendKeys("Password@12345");
     // Select login button and invoke click action
     //If login details are correct we wiil be redirected to the welcome page
@@ -19,6 +19,7 @@ async function loginTest() {
     assert.strictEqual(pageTitle, "Welcomepage");
     //Check if redirect to login page was successfull
     await driver.wait(until.titleIs("Welcomepage"), 4000);
+    */
   } finally {
     await driver.quit();
   }
