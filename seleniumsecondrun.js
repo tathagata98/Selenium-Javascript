0c8aae5337c638c9985aa58944a3d3ff31096470
Tathagata Pal <tathagatapal12@gmail.com>
2024-03-18 19:59:29 +0530

    push

--- seleniumsecondrun.js
+++ seleniumsecondrun.js
@@ -2,7 +2,8 @@
 // program to generate fibonacci series up to n terms
 
 // take input from the user
-const number = parseInt(prompt('Enter the number of terms: '));
+const number = 10;
+//parseInt(prompt('Enter the number of terms: '));
 let n1 = 0, n2 = 1, nextTerm;
 
 console.log('Fibonacci Series:');
